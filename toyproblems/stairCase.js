8a063ebed9bd5a498179d5bf627ed00a210792e8
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-01-24 21:44:13 +0300

    Assignment Done

--- toyproblems/stairCase.js
+++ toyproblems/stairCase.js
@@ -2,7 +2,7 @@ function steps(n) {
   for (let i = 1; i <= n; i++) {
     let stair = "";
 
-    // Add '#' characters
+    // Add '# ' characters
     for (let j = 1; j <= i; j++) {
       stair += "#";
     }
